dd2cbfd5c16227f2247c5d0996ab5518d33b6ad7
Sumit Salpekar <141239784+ssalpekar@users.noreply.github.com>
2024-01-25 16:21:50 +0530

    Create PardotParameters.cs

---
+++ Sitecore Forms/CustomSubmitActions/Pardot/View Models/PardotParameters.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumitsProject.Foundation.Pardot
+{
+    public class PardotParameters
+    {
+        public string EndpointUrl { get; set; }
+    }
+}
